bf2ebad33106661832e5e7ea7c43eb381826b8dd
Unigwe <emmunigwe@gmail.com>
2025-02-28 15:55:12 +0100

    feat(core): setup flask project

---
+++ AProject/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./templates/**/*.html",
+        "./static/css/**/*.css"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+}
+  
